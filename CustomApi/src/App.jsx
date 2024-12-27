357a71533189172bcc855bab323bcb6a5f79821d
Hemant0621 <hemant.kumar.2335.h@gmail.com>
2024-12-27 17:43:03 +0530

    eit

--- CustomApi/src/App.jsx
+++ CustomApi/src/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainWebsite from './components/Home';
 import AdminPanel from './components/Admin';
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainWebsite />} />
-        <Route path="/admin" element={<AdminPanel />} />
-      </Routes>
-    </BrowserRouter>
+    <AdminPanel />
   );
 }
 
