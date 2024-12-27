5a0052d679d0b374576b690ec5ec246be3465e11
Hemant0621 <hemant.kumar.2335.h@gmail.com>
2024-12-27 18:18:51 +0530

    ..

--- CustomApi/src/components/Admin.jsx
+++ CustomApi/src/components/Admin.jsx
@@ -131,7 +131,7 @@ function AdminPanel() {
             }));
 
         try {
-            const response = await axios.post(`${Backend_url}admin/apis`,apiNodes);
+            const response = await axios.post(`${Backend_url}admin/apis`, apiNodes);
 
             if (response.data.message) {
                 alert("API Flow saved successfully");
@@ -147,12 +147,11 @@ function AdminPanel() {
         }
 
         try {
-            const response = await axios.post(`${Backend_url}admin/connections`, {
+            const response = await axios.post(`${Backend_url}admin/connections`, body: JSON.stringify({ nodes, edges }), {
                 headers: {
                     'Content-Type': 'application/json',
                     "Access-Control-Allow-Origin": "*"
-                },
-                body: JSON.stringify({ nodes, edges }),
+                }
             });
 
             if (response.ok) {
